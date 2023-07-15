ef9e365acab6d42e663dc28c2c97c513203ca767
Paranjothi R <paranjothi502@gmail.com>
2023-07-15 11:12:57 +0530

    initial

---
+++ sensor.js
@@ -0,0 +1,11 @@
+const express=require("express")
+const app=express()
+
+const port=process.env.PORT || 3000
+ 
+app.post('/',(req,res)=>{
+    
+})
+app.listen(port,()=>{
+    console.log(`Server is running ${port}`)
+})
