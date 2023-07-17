bcc187165308425bcd1e57f54236d2f2915c8da0
Paranjothi R <paranjothi502@gmail.com>
2023-07-18 00:41:33 +0530

    db2

--- sensor.js
+++ sensor.js
@@ -1,28 +1,41 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
 require('dotenv').config();
-const mongoose=require("mongoose")
-const url=process.env.URL
+const mongoose = require("mongoose");
+const dbstore = require("./Model/sensorvalue");
 
-const port=process.env.PORT
+const url = process.env.URL;
+const port = process.env.PORT;
 
-mongoose.connect(url,{useNewUrlParser:true})
-const cons=mongoose.connection
-cons.on('open',()=>{
-     console.log("connect...")
-})
-const dbstore=require("./Model/sensorvalue")
-app.use(express.json())
-app.post('/api/sensordata',async(req, res)=> {
-        const { temperature, waterLevelPercentage, acState, acno, deviceid } = req.body;
-        if (deviceid === "#1A6B9C") {
-            console.log(req.body);
-            
-                const dataToSave = await dbstore.save();
-                console.log("sucess")
+mongoose.connect(url, { useNewUrlParser: true });
+const cons = mongoose.connection;
+cons.on('open', () => {
+    console.log("Connected to MongoDB...");
+});
+
+app.use(express.json());
+app.post('/api/sensordata', async (req, res) => {
+    const { temperature, waterLevelPercentage, acState, acno, deviceid } = req.body;
+    if (deviceid === "#1A6B9C") {
+        console.log(req.body);
+        try {
+            const newSensorData = new dbstore({
+                temperature,
+                waterLevelPercentage,
+                acState,
+                acno,
+                deviceid
+            });
+            const dataToSave = await newSensorData.save();
+            console.log("success");
+        } catch (error) {
+            console.error(error);
         }
-        res.status(500).json({ message: "get the post sucessfully" });
-    })
-app.listen(port,()=>{
-    console.log(`Server is running ${port}`)
-})
+    }
+
+    res.status(200).json({ message: "get the post successfully" });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
