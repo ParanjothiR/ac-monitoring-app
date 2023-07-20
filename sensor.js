8c4803cde5dcfb2bb62b39c2f0299ce0f27d7f59
Paranjothi R <paranjothi502@gmail.com>
2023-07-20 20:31:14 +0530

    tokens

--- sensor.js
+++ sensor.js
@@ -1,7 +1,11 @@
 const express = require("express");
+
 const app = express();
 require('dotenv').config();
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+
+
 //const dbstore = require("./Model/sensorvalue");
 const url = process.env.URL;
 const port = process.env.PORT;
@@ -11,8 +15,8 @@ const cons = mongoose.connection;
 cons.on('open', () => {
     console.log("Connected to MongoDB...");
 });
-
-app.use(express.json());
+app.use(bodyParser.json());
+//app.use(express.json());
 app.use("/api/sensordata",require("./Router/sensorroutes"))
 app.use("/api/users",require("./Router/userrouter"))
 
