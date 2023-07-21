6cd05c75e92241e63888e1e9b3dc7d49c62f04f2
Paranjothi R <122994240+ParanjothiR@users.noreply.github.com>
2023-07-21 21:32:13 +0530

    Update sensorroutes.js

--- Router/sensorroutes.js
+++ Router/sensorroutes.js
@@ -15,7 +15,7 @@ router.post('/',async (req, res) => {
     if (deviceid === "#1A6B9C"){
         // console.log(req.body);
         try {
-            const newSensorData = await new dbstore({
+            const newSensorData = new dbstore({
                 temperature,
                 waterLevelPercentage,
                 acState,
