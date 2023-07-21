b04d6432bd4bb825b0d223bc39f2005dffd03290
Paranjothi R <paranjothi502@gmail.com>
2023-07-21 20:42:50 +0530

    time

--- Router/sensorroutes.js
+++ Router/sensorroutes.js
@@ -6,9 +6,16 @@ const validatesensordata = require("../Middleware/validatesensordata");
 const validateToken=require("../Middleware/validateToken")
 
 router.use(express.json())
+
+router.get('/get',async(req,res)=>{
+    //dbstore.deleteMany({ deviceid: req.deviceid })
+    const s=dbstore.find()
+    console.log(s)
+    res.send("hii")
+})
 router.post('/',async (req, res) => {
     const { temperature, waterLevelPercentage, acState, acno, deviceid } = req.body;
-    const timestamp = new Date();
+    const timestamp = new Date().toLocaleString('en-US', {timeZone: 'Asia/Kolkata'});
     
     if (deviceid === "#1A6B9C"){
         // console.log(req.body);
@@ -24,7 +31,7 @@ router.post('/',async (req, res) => {
            //const data=newSensorData
             // console.log(newSensorData)
 
-            const dataToSave = await newSensorData.save();
+            const dataToSave = newSensorData.save();
             console.log(dataToSave)
             validatesensordata(req,dataToSave)
         } catch (error) {
